446a7632d3485451afa6df4284c0b7d61b57910c
LLwassim <53097038+LLwassim@users.noreply.github.com>
2019-07-31 17:16:42 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,11 @@
-console.log("Up and running!");
-var cardOne = "queen";
-var cardTwo = "king";
-var cardThree = "queen";
-var cardFour = "king";
-console.log("User flipped " + cardOne)
-console.log("User flipped " + cardTwo)
+
+let cards = ['Queen','queen','King','king'];
+let cardsInPlay = [];
+var cardOne = cards[0];
+cardsInplay = cards.push(cardOne);
+console.log('user flipped ' + cards[0])
+var cardTwo = cards[2];
+cardsInPlay = cards.push(cardTwo);
+console.log('user flipped ' + cards[2])
+if(cardsInplay.lenght === 2)
+	console.log('flip')
