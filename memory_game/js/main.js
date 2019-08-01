bc8a9e55508aef48d2cf7ce9bfd3003a48aaa099
LLwassim <53097038+LLwassim@users.noreply.github.com>
2019-08-01 18:15:20 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,11 +1,19 @@
 
-let cards = ['Queen','queen','King','king'];
+let cards = 
+['Queen','queen','King','king'];
 let cardsInPlay = [];
 var cardOne = cards[0];
 cardsInplay = cards.push(cardOne);
 console.log('user flipped ' + cards[0])
 var cardTwo = cards[2];
 cardsInPlay = cards.push(cardTwo);
+console.log('user flipped ' + cards[1]) 
+var checkForMatch = function() {
 console.log('user flipped ' + cards[2])
-if(cardsInplay.lenght === 2)
-	console.log('flip')
+if(cardsInPlay[0] === cardsInPlay[1]) { 
+	alert('You found a match');
+}
+ else {
+ 	alert("Try again");
+ }
+};
